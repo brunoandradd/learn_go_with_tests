20a0654138d4dea1716e32bc566d4c5595988293
brunoandradd <bruno77sa@gmail.com>
2024-07-21 20:53:07 -0300

    feat: finish study about slices and arrays

--- arrays_slices/sum.go
+++ arrays_slices/sum.go
@@ -1,9 +1,46 @@
 package arraysslices
 
+import "fmt"
+
 func Sum(list []int) int {
 	var total int
 	for _, value := range list {
 		total += value
 	}
 	return total
+}
+
+func SumAll(numbersTosum ...[]int) []int {
+	var result []int
+
+	for _, numbers := range numbersTosum {
+		result = append(result, Sum(numbers))
+	}
+	return result
+}
+
+var puts = fmt.Println
+
+func SumAllTails(numbersTosum ...[]int) []int {
+	var result []int
+
+	for _, numbers := range numbersTosum {
+		if size := len(numbers); size == 0 {
+			result = append(result, 0)
+		} else {
+			tail := numbers[1:]
+			result = append(result, Sum(tail))
+		}
+	}
+	return result
+}
+
+func SumAllWithIndex(numbersTosum ...[]int) []int {
+	arrayCount := len(numbersTosum)
+	result := make([]int, arrayCount)
+
+	for i := range arrayCount {
+		result[i] = Sum(numbersTosum[i])
+	}
+	return result
 }
