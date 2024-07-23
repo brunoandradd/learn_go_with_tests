15d2d06feebd04a768bf2ff96438a4ab2d3eac2d
brunoandradd <bruno77sa@gmail.com>
2024-07-22 23:09:45 -0300

    feat: implements structs and  interfaces with tests

--- structs/struct_test.go
+++ structs/struct_test.go
@@ -12,12 +12,44 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
-func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
-
+var checkArea = func(t testing.TB, shape Shape, want float64) {
+	t.Helper()
+	got := shape.Area()
 	if got != want {
-		t.Errorf("got %.2f, want %.2f", got, want)
+		t.Errorf("got %g want %g", got, want)
 	}
 }
+
+func TestArea(t *testing.T) {
+	t.Run("retangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		want := 72.0
+
+		checkArea(t, rectangle, want)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10.0}
+		want := 314.1592653589793
+
+		checkArea(t, circle, want)
+	})
+}
+
+func TestAreaTable(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
+	}
+
+	for _, tt := range areaTests {
+		checkArea(t, tt.shape, tt.want)
+	}
+}
+
+// continue here
+// https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/structs-methods-and-interfaces#make-sure-your-test-output-is-helpful
